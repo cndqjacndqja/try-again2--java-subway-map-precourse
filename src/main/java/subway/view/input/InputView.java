261b6afcf494dab77b1d80b6903e9d01edb839ff
jochungbeom <cndqjacndqja@gmail.com>
2021-12-16 18:08:11 +0900

    feat: 등록할 노선의 하행 종점역 이름 입력

--- src/main/java/subway/view/input/InputView.java
+++ src/main/java/subway/view/input/InputView.java
@@ -40,4 +40,9 @@ public class InputView {
 		System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
 		return scanner.nextLine();
 	}
+
+	public static String inputEndStationForLineRegister(Scanner scanner) {
+		System.out.println("## 등록할 노선의 하행 종점역 이름을 입력하세요.");
+		return scanner.nextLine();
+	}
 }
