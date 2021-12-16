179cb5d4a6c5f7e3dd50e414a9aa036d30ce784c
jochungbeom <cndqjacndqja@gmail.com>
2021-12-16 19:03:04 +0900

    feat: 구간 관리 수행 중, 오류가 발생했을 때 다시 재실행 구현

--- src/main/java/subway/controller/SectionController.java
+++ src/main/java/subway/controller/SectionController.java
@@ -1,12 +1,21 @@
 package subway.controller;
 
+import static subway.view.input.InputView.*;
 import static subway.view.output.OutputView.*;
 
 import java.util.Scanner;
 
+import subway.controller.function.SectionFunction;
+
 public class SectionController {
 	public void run(Scanner scanner) {
 		printSectionScreen();
-
+		try{
+			SectionFunction sectionFunction = SectionFunction.findFunction(inputLineFunction(scanner));
+			sectionFunction.apply(scanner);
+		}catch (IllegalArgumentException illegalArgumentException) {
+			System.out.println(illegalArgumentException.getMessage());
+			run(scanner);
+		}
 	}
 }
