7625c554f39f715acfcaf9913128d227c69f43f2
jochungbeom <cndqjacndqja@gmail.com>
2021-12-16 17:27:04 +0900

    feat: 역 관리 기능 입력

--- src/main/java/subway/view/input/InputView.java
+++ src/main/java/subway/view/input/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
 		System.out.println("원하는 기능을 입력하세요.");
 		return scanner.nextLine();
 	}
+
+	public static String inputStationFunction(Scanner scanner) {
+		System.out.println("원하는 기능을 입력하세요.");
+		return scanner.nextLine();
+	}
 }
