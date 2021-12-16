55977eef93e457185b6d4ad449760d48fb0737c8
jochungbeom <cndqjacndqja@gmail.com>
2021-12-16 18:14:23 +0900

    feat: 지하철 노선 등록하기

--- src/main/java/subway/controller/function/LineFunction.java
+++ src/main/java/subway/controller/function/LineFunction.java
@@ -1,12 +1,22 @@
 package subway.controller.function;
 
+import static subway.view.input.InputView.*;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
 
 public enum LineFunction {
 	REGISTER("1") {
 		@Override
 		void function(Scanner scanner) {
+			String lineName = inputLineNameForLineRegister(scanner);
+			Station startStation = new Station(inputStartStationForLineRegister(scanner));
+			Station endStation = new Station(inputEndStationForLineRegister(scanner));
+			LineRepository.addLine(new Line(lineName, startStation, endStation));
 
 		}
 	},
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,16 @@
 package subway.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stationList = new LinkedList<>();
 
-    public Line(String name) {
+    public Line(String name, Station startStation, Station endStation) {
         this.name = name;
+        stationList.add(startStation);
+        stationList.add(endStation);
     }
 
     public String getName() {
