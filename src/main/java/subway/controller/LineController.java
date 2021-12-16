34cc3bbce5f40ed177aeb6201661be2d74440bdd
jochungbeom <cndqjacndqja@gmail.com>
2021-12-16 17:58:56 +0900

    feat: 노선 관리 화면 출력

---
+++ src/main/java/subway/controller/LineController.java
@@ -0,0 +1,11 @@
+package subway.controller;
+
+import static subway.view.output.OutputView.*;
+
+import java.util.Scanner;
+
+public class LineController {
+	public void run(Scanner scanner) {
+		printLineScreen();
+	}
+}
--- src/main/java/subway/view/output/OutputView.java
+++ src/main/java/subway/view/output/OutputView.java
@@ -41,4 +41,13 @@ public class OutputView {
 		}
 		printEmptyLine();
 	}
+
+	public static void printLineScreen() {
+		System.out.println("## 노선 관리 화면");
+		System.out.println("1. 노선 등록");
+		System.out.println("2. 노선 삭제");
+		System.out.println("3. 노선 조회");
+		System.out.println("B. 돌아가기");
+		printEmptyLine();
+	}
 }
